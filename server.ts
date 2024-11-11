5b963be90313f58d848fa86edcc82bd5a9e618dc
Al Duncanson <alDuncanson@proton.me>
2024-11-10 22:54:50 -0500

    Add cache control

--- server.ts
+++ server.ts
@@ -5,7 +5,20 @@ Bun.serve({
     const url = new URL(req.url);
     if (url.pathname === "/") return new Response(Bun.file("./index.html"));
     if (url.pathname === "/messages/logo.js")
-      return new Response(Bun.file("./messages/logo.js"));
+      return new Response(Bun.file("./messages/logo.js"), {
+        headers: {
+          "Content-Type": "application/javascript",
+          "Cache-Control": "max-age=31536000",
+        },
+      });
+    if (url.pathname === "/messages/hiring.js")
+      return new Response(Bun.file("./messages/hiring.js"), {
+        headers: {
+          "Content-Type": "application/javascript",
+          "Cache-Control": "max-age=31536000",
+        },
+      });
+
     if (url.pathname === "/robots.txt")
       return new Response(Bun.file("./robots.txt"));
     return new Response("404!");
